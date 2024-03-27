9e0550d2a56e8813b3aa35bf22efd6c9b87d0384
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-03-27 14:15:56 +0200

    Update betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,18 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of node.
+ * binary_tree_delete -  deletes an entire binary tree.
  * @tree: the root node pointer
- *
- * Return: Pointer to the node.
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
+
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
+
 	free(tree);
-	return;
 }
