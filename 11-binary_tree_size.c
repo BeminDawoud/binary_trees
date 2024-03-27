b81d3fb8eaa8ca279319115c68866ee8d900a5fe
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-03-27 17:02:08 +0200

    Update

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,17 +3,18 @@
 /**
  * binary_tree_size -  measures the size of a binary tree.
  * @tree: the root node pointer
+ * Return: size of the tree
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	static size_t size = 0;
 
 	if (tree == NULL)
 		return (size);
+	size++;
 	binary_tree_size(tree->left);
 	binary_tree_size(tree->right);
-	size++;
 
 	return (size);
 }
