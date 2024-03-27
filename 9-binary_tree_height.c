3ccbab393a34451a040e8c85dd9de2e1c30c6ab2
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-03-27 17:24:19 +0200

    Update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,20 +7,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	static size_t height_l, height_r;
+	size_t height_l, height_r;
 
 	if (tree == NULL)
-		return ((height_l > height_r) ? height_l : height_r);
+		return (0);
 
 	if (tree->left)
 	{
-		height_l++;
-		binary_tree_height(tree->left);
+		height_l = 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right)
 	{
-		height_r++;
-		binary_tree_height(tree->right);
+		height_r = 1 + binary_tree_height(tree->right);
 	}
 
 	return ((height_l > height_r) ? height_l : height_r);
